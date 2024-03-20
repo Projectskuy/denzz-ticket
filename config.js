2e4fa943f15145a438f85db5c6b3b75d94f8088e
DenzzForTeams <96470753+Projectskuy@users.noreply.github.com>
2024-03-21 03:48:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     modmail: {
-        ownerID: '', // ← Insert the Server Owner ID.
+        ownerID: '1207745002650861623', // ← Insert the Server Owner ID.
         supportId: '', // ← Insert General Support Category ID.
         premiumId: '', // ← Insert Premium Support Category ID.
-        whitelist: '', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
-        staff: '', // ← Insert Staff Role to be pinged upon ticket creation.
+        whitelist: '1220104889200545882', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
+        staff: '1220104889200545882', // ← Insert Staff Role to be pinged upon ticket creation.
     },
     logs: {
-        logschannel: '' // ← Insert Ticket Logs Channel ID.
+        logschannel: '1220111486295474268' // ← Insert Ticket Logs Channel ID.
     }
 };
