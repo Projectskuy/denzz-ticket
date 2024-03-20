b3a04b096ea92eb738992937db4a92543bb9db3f
DenzzForTeams <96470753+Projectskuy@users.noreply.github.com>
2024-03-21 03:50:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     modmail: {
         ownerID: '1207745002650861623', // ← Insert the Server Owner ID.
-        supportId: '', // ← Insert General Support Category ID.
-        premiumId: '', // ← Insert Premium Support Category ID.
+        supportId: '1220087861903228976', // ← Insert General Support Category ID.
+        premiumId: '1220087861760753734', // ← Insert Premium Support Category ID.
         whitelist: '1220104889200545882', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
         staff: '1220104889200545882', // ← Insert Staff Role to be pinged upon ticket creation.
     },
